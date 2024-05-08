84dc3253d0b882c1de2cd4ee7833c5c03b03b81c
123100123 <146941456+123100123@users.noreply.github.com>
2024-05-08 08:52:34 +0330

    base classes

---
+++ userController.py
@@ -0,0 +1,29 @@
+from user import User
+
+class UserController:
+    def __init__(self) -> None:
+        pass
+
+    def exists(self, username: str) -> bool:
+        pass
+    
+    @staticmethod
+    def add_user(self,user_name:str)->None:
+        pass
+    
+    @staticmethod
+    def remove_user(self,username:str)->None:
+        pass
+    
+    @staticmethod
+    def get_user(self,username:str)->User:
+        pass
+    
+    @staticmethod
+    def get_users(self) -> list[str]:
+        pass
+        
+    
+    
+    
+    
