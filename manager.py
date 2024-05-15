3149f7c9fb7a988b07ebdd1090c65930d1ff9d27
123100123 <146941456+123100123@users.noreply.github.com>
2024-05-15 18:09:04 +0330

    action in manager
    added a parser for the desired action of the user

--- manager.py
+++ manager.py
@@ -2,23 +2,25 @@ from encoder import Encoder
 import argparse
 import os
 
-encoder = Encoder()
-
 
 def create_admin(username, password):
     if os.path.exists("Admin.txt"):
         print("Error: Admin Already Exists")
+        exit(1)
 
-    output = "%s,%s" % (username, encoder.encrypt(password).decode())
+    output = "%s,%s" % (username, Encoder.encrypt(password))
     with open("Admin.txt", "w") as file:
         file.write(output)
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--username", type=str, default="admin", help= "defines the username of the admin")
+parser.add_argument("action",type=str)
+parser.add_argument("--username", type=str, default="admin", help= "defines the username of the admin",required=True)
 parser.add_argument("--password", type=str, default="admin", help="defines the password of the admin")
 
 args = parser.parse_args()
 
-
-create_admin(args.username, args.password)
+if args.action == "create-admin":
+    create_admin(args.username, args.password)
+else:
+    print("Error: Unknown action. Use 'create-admin' to create an admin user.")
